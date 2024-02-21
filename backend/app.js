4e4adef056ebe5900237e6d067f563fd612e934e
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-21 20:28:35 +0100

    mongoose installed & function to connect w/ MongoDB Atlas

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import routerApi from './server/routes/routerApiWorkouts.js'
+import connectDB from './server/config/db.js'
 
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
+
+// Connect MongoDB
+connectDB()
 
 // Routers
 app.use('/api/workouts', routerApi)
