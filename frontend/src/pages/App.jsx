89cdaada41674b2c3b41560f956bfa2a97cd043c
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-22 12:04:52 +0100

    Fetching data of GET all workouts (CORS error)

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react"
 
 const App = () => {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+
+    const fetchWorkoutData = async () => {
+
+      const response = await fetch('http://localhost:3000/api/workouts')
+      const json = await response.json()
+
+      if(response.ok) {
+        setWorkouts(json)
+      }
+
+    }
+    fetchWorkoutData()
+
+  }, [])
+  
+
   return (
-    <>
-    </>
+    <div className="home">
+
+      <div className="workouts">
+
+        {workouts.map((workout) => (
+
+          <p key={workout._id}>{workout.title}</p>
+
+        ))}
+
+      </div>
+
+    </div>
   )
 }
 
