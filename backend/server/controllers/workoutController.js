47202d1fe191cd7cfd71159ee47eff038ccbfd49
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-22 10:23:18 +0100

    GET all workouts function & route

--- backend/server/controllers/workoutController.js
+++ backend/server/controllers/workoutController.js
@@ -1,5 +1,20 @@
 import Workout from "../models/WorkoutModel.js";
 
+export const getAllWorkouts = async (_req, res) => {
+
+  try {
+    
+    const workouts = await Workout.find({}).sort({createdAt: -1}) // Descending order
+    res.json(workouts).status(200)
+
+  } catch (error) {
+   
+    res.json({ error: error.message }).status(500)
+
+  }
+
+}
+
 export const createWorkout = async (req, res) => {
 
   const { title, reps, load } = req.body
--- backend/server/routes/routerApiWorkouts.js
+++ backend/server/routes/routerApiWorkouts.js
@@ -1,13 +1,9 @@
 import express from 'express'
-import { createWorkout } from '../controllers/workoutController.js'
+import { getAllWorkouts, createWorkout } from '../controllers/workoutController.js'
 
 const router = express.Router()
 
-router.get('/', (_req, res) => {
-
-  res.send('API test')
-
-})
+router.get('/', getAllWorkouts)
 
 router.post('/', createWorkout)
 
