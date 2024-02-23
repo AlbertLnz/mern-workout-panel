14b29921d7ac8dc1a240bc9b080d526f61ea01bd
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-23 16:37:30 +0100

    Auth context set up for all application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,20 +5,23 @@ import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar.jsx'
 import { WorkoutsContextProvider } from './context/WorkoutContext.jsx'
+import { AuthContextProvider } from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <WorkoutsContextProvider>
-      <div className='App'>
-        <BrowserRouter>
-          <NavBar />
-          <div className='pages'>
-            <Routes>
-              <Route path="/" element={ <App /> }/>
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </div>
-    </WorkoutsContextProvider>
+    <AuthContextProvider>
+      <WorkoutsContextProvider>
+        <div className='App'>
+          <BrowserRouter>
+            <NavBar />
+            <div className='pages'>
+              <Routes>
+                <Route path="/" element={ <App /> }/>
+              </Routes>
+            </div>
+          </BrowserRouter>
+        </div>
+      </WorkoutsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
 )
