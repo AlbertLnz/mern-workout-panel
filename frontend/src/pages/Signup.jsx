367d2cc38fba9d9518457db49efa5a627692500a
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-26 19:59:45 +0100

    Exporting Signup functionability to a customm hook useSignup(signup, error, isLoading)

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,49 +1,19 @@
 import { useState } from "react";
-import useAuthContext from '../hooks/useAuthContext.js'
+import { useSignup } from "../hooks/useSignup";
 
 const Signup = () => {
   
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(null);
-  const { dispatch } = useAuthContext()
+  const {signup, error, isLoading} = useSignup()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
 
     // console.log(email, password)
 
-    const signup = async (email, password) => {
-      setIsLoading(true)
-      setError(null)
-
-      const response = await fetch('http://localhost:3000/auth/signup', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-      })
-
-      const json = await response.json()
-
-      if(!response.ok) {
-        setIsLoading(false)
-        setError(json.error)
-      }
-
-      if(response.ok) {
-        localStorage.setItem('user', JSON.stringify(json.user)) // save the user to local storage
-        dispatch({ type: 'SIGNUP', payload: json.user }) // update the userContext
-
-        setIsLoading(false)
-      }
-    }
-
     await signup(email, password)
-
 
   }
 
